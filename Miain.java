66eeb7fe0434f3e545fc085f16fdab6462140f42
Andrew Matysiak <andrewmatysiak@hotmail.com>
2022-10-08 09:54:25 +1100

    Initial commit

---
+++ Miain.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Miain {
+
+    public static void main (String[] args){
+
+        // enter map layout
+        //Scanner keyboard = new Scanner(System.in);
+        //String mapLayout = keyboard.nextLine();
+
+        // map layouts for testing
+        //String mapLayout = "xxxxxxxxxGxxxx$xxxT";
+        String mapLayout = "xxxxxTxxxxGxxxxxxxx$xx";
+
+
+
+
+    }
+}
