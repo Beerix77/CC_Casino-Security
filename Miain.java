91ad0188c7dd4b116c54046ddd4310330c17a2dc
Andrew Matysiak <andrewmatysiak@hotmail.com>
2022-10-08 10:02:19 +1100

    Commit2 W.I.P.

--- Miain.java
+++ Miain.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Miain {
@@ -12,8 +13,19 @@ public class Miain {
         //String mapLayout = "xxxxxxxxxGxxxx$xxxT";
         String mapLayout = "xxxxxTxxxxGxxxxxxxx$xx";
 
+        // convert map layout to Array and locate index of $
+        String[] arrayMap = mapLayout.split("");
+        System.out.println(Arrays.toString(arrayMap));
 
 
+        // TODO: add condition for: Multiple $, T, G present or missing
 
+        int indexMoney = 0;
+        for (int i = 0; i <= arrayMap.length-1; i++) {
+            if (arrayMap[i].equals("$")) {
+                indexMoney = i;
+            }
+        }
+        System.out.println(indexMoney);
     }
 }
